9dea7dd6105a692c45e500a1998dd5f0da9e5e1e
mhesselbarth <mhk.hesselbarth@gmail.com>
2021-02-04 19:21:45 -0500

    :art: Update plots full desing

--- 03_Analysis/03_run_model/03d_run_model.R
+++ 03_Analysis/03_run_model/03d_run_model.R
@@ -57,16 +57,25 @@ biomass_attr <- purrr::map_dfr(model_runs_attr, function(i) readr::read_rds(i) %
                                  calc_total_biomass(), .id = "id_sim")
 
 # combine both movement behaviors
-biomass_combined <- dplyr::bind_rows(biomass_rand, biomass_attr, .id = "id_move") %>% 
+biomass_combined <- dplyr::bind_rows(rand = biomass_rand, attr = biomass_attr, 
+                                     .id = "id_move") %>% 
   dplyr::left_join(sim_experiment, by = "id_sim") %>% 
   dplyr::filter(timestep == max(timestep)) %>% 
   dplyr::select(-c(timestep, id_sim)) %>%
-  dplyr::mutate(id_move = factor(id_move, levels = c("1", "2"), 
-                                 labels = c("Random movement", "Attracted movement")), 
+  dplyr::mutate(id_move = factor(id_move, levels = c("rand", "attr")),  
                 part = factor(part, levels = c("ag_biomass", "bg_biomass")), 
                 nutrients_pool = factor(nutrients_pool, ordered = TRUE),
                 pop_n = factor(pop_n, ordered = TRUE), 
                 id = 1:nrow(.))
+
+# get mean and sd of repetitions
+biomass_combined_sum <- dplyr::group_by(biomass_combined, id_move, part, nutrients_pool, pop_n) %>% 
+  dplyr::summarise(mean = mean(biomass), sd = sd(biomass), .groups = "drop") %>%
+  dplyr::mutate(low = mean - sd, high = mean + sd) %>% 
+  dplyr::mutate(part = factor(part, labels = c("Aboveground biomass", 
+                                               "Belowground biomass")), 
+                id_move = factor(id_move, labels = c("No influence of AR", 
+                                                     "Attracted towards AR")))
 
 #### Calculate total production ####
 
@@ -79,16 +88,24 @@ production_attr <- purrr::map_dfr(model_runs_attr, function(i) readr::read_rds(i
                                     calc_total_production(), .id = "id_sim")
 
 # combine both movement behaviors
-production_combined <- dplyr::bind_rows(production_rand, production_attr, .id = "id_move") %>% 
+production_combined <- dplyr::bind_rows(rand = production_rand, attr = production_attr, 
+                                        .id = "id_move") %>% 
   dplyr::left_join(sim_experiment, by = "id_sim") %>% 
   dplyr::filter(timestep == max(timestep)) %>% 
   dplyr::select(-c(timestep, id_sim)) %>%
-  dplyr::mutate(id_move = factor(id_move, levels = c("1", "2"), 
-                                 labels = c("Random movement", "Attracted movement")), 
+  dplyr::mutate(id_move = factor(id_move, levels = c("rand", "attr")), 
                 part = factor(part, levels = c("ag_production", "bg_production")),
                 nutrients_pool = factor(nutrients_pool, ordered = TRUE),
                 pop_n = factor(pop_n, ordered = TRUE), 
                 id = 1:nrow(.))
+
+production_combined_sum <- dplyr::group_by(production_combined, id_move, part, nutrients_pool, pop_n) %>% 
+  dplyr::summarise(mean = mean(production), sd = sd(production), .groups = "drop") %>%
+  dplyr::mutate(low = mean - sd, high = mean + sd) %>% 
+  dplyr::mutate(part = factor(part, labels = c("Aboveground production", 
+                                               "Belowground production")), 
+                id_move = factor(id_move, labels = c("No influence of AR", 
+                                                     "Attracted towards AR")))
 
 #### Normalize values ####
 
@@ -122,69 +139,158 @@ anova_production <- dplyr::group_by(production_norm, part) %>%
 
 anova_production_sum <- purrr::map(anova_production, summary)
 
-# #### Plot low vs high nutrients ####
-# 
-# # load random movement data
-# result_rand <- purrr::map_dfr(model_runs_rand, function(i) readr::read_rds(i) %>%
-#                                 magrittr::extract2("seafloor") %>% 
-#                                 calc_total_production(), .id = "id_sim")
-# 
-# # load attracted movement data
-# result_attr <- purrr::map_dfr(model_runs_attr, function(i) readr::read_rds(i) %>% 
-#                                 magrittr::extract2("seafloor") %>% 
-#                                 calc_total_production(), .id = "id_sim")
-# 
-# # combine to one data.frame
-# result_combined <- dplyr::bind_rows(result_rand, result_attr, .id = "id_move") %>% 
-#   dplyr::left_join(sim_experiment, by = "id_sim") %>% 
-#   dplyr::group_by(id_move, timestep, part, nutrients_pool, pop_n) %>% 
-#   dplyr::summarise(mean = mean(production), sd = sd(production), .groups = "drop") %>% 
-#   dplyr::mutate(id_move = factor(id_move, levels = c("1", "2"), 
-#                                  labels = c("Random movement", "Attracted movement")),
-#                 part = factor(part, levels = c("ag_production", "bg_production"), 
-#                               labels = c("Aboveground production", "Belowground production")), 
-#                 nutrients_pool = factor(nutrients_pool, ordered = TRUE),
-#                 pop_n = factor(pop_n, ordered = TRUE), 
-#                 timestep = (timestep * min_per_i) / 60 / 24 / 365) %>% 
-#   dplyr::mutate(low = mean - sd, high = mean + sd) %>% 
-#   dplyr::select(-sd) %>% 
-#   tidyr::pivot_wider(names_from = id_move, values_from = c(mean, low, high)) %>% 
-#   purrr::set_names(names(.) %>% 
-#                      stringr::str_replace(pattern = " ", replacement = "_") %>% 
-#                      stringr::str_to_lower()) %>% 
-#   dplyr::mutate(diff_low = ((low_attracted_movement - low_random_movement) / low_random_movement * 100),
-#                 diff_mean = ((mean_attracted_movement - mean_random_movement) / mean_random_movement * 100), 
-#                 diff_high = ((high_attracted_movement - high_random_movement) / high_random_movement * 100)) %>% 
-#   dplyr::select(-tidyselect::starts_with(c("low_", "mean_", "high_"))) %>% 
-#   tidyr::replace_na(list(diff_low = 0, diff_mean = 0, diff_high = 0))
-# 
-# gg_production_nutr <- ggplot(data = result_combined) +
-#   # geom_ribbon(aes(x = timestep, ymin = diff_low, ymax = diff_high, fill = pop_n), alpha = alpha) +
-#   geom_line(aes(x = timestep, y = diff_mean, col = pop_n)) +
-#   geom_vline(xintercept = burn_in, linetype = 3, col = "grey") +
-#   geom_hline(yintercept = 0, linetype = 2, col = "lightgray") +
-#   facet_wrap(~part + nutrients_pool, scales = "free_y", nrow = 2, ncol = 5) +
-#   scale_fill_viridis_d(name = "Fish population size") +
-#   scale_color_viridis_d(name = "Fish population size") +
-#   guides(colour = guide_legend(nrow = 1), 
-#          fill = guide_legend(nrow = 1)) +
-#   labs(x = "Years", y = expression(paste(Delta, Sigma, Prdocution[rand - attr], " (dry) [%]"))) + 
-#   theme_classic(base_size = base_size) +
+#### Delta treatment vs null #### 
+
+gg_biomass_full_design_sum <- ggplot(biomass_combined_sum, 
+                                     aes(x = pop_n, y = mean)) + 
+  geom_line(aes(group = interaction(id_move, nutrients_pool), linetype = id_move), 
+            col = "lightgrey") +
+  geom_point(aes(col = nutrients_pool, shape = id_move), size = 1.5) +
+  facet_wrap(~ part, scales = "free_y", ncol = 2) + 
+  scale_color_manual(name = "Water column nutrients pool",
+                     values = c("#ECCBAE", "#046C9A", "#D69C4E", "#ABDDDE", "#000000")) +
+  scale_shape_manual(name = "Hypothesis", values = c(1, 15)) +
+  scale_linetype_manual(name = "Hypothesis", values = c(2, 1)) +
+  labs(x = "Number of fish individuals", 
+       y = expression(paste(Sigma, Biomass, " (dry) [g]"))) + 
+  theme_classic(base_size = 10) + 
+  theme(legend.position = "bottom")
+
+biomass_combined_diff <- dplyr::group_by(biomass_combined, id_move, part, nutrients_pool, pop_n) %>% 
+  dplyr::summarise(mean = mean(biomass), sd = sd(biomass), .groups = "drop") %>%
+  dplyr::mutate(low = mean - sd, high = mean + sd) %>% 
+  tidyr::pivot_wider(-sd, names_from = id_move, values_from = c(low, mean, high)) %>% 
+  purrr::set_names(names(.) %>% 
+                     stringr::str_replace_all(pattern = " ", replacement = "_") %>% 
+                     stringr::str_to_lower()) %>% 
+  dplyr::mutate(low_abs = low_attr - low_rand,
+                mean_abs = mean_attr - mean_rand, 
+                high_abs = high_attr - high_rand, 
+                low_rel = low_abs / low_rand * 100,
+                mean_rel = mean_abs / mean_rand * 100, 
+                high_rel = high_abs / high_rand * 100) %>% 
+  dplyr::mutate(part = factor(part, labels = c("Aboveground biomass", 
+                                               "Belowground biomass")))
+  
+gg_biomass_full_design_diff <- ggplot(biomass_combined_diff, 
+                                      aes(x = pop_n, y = mean_rel)) + 
+  geom_line(aes(group = nutrients_pool), alpha = 0.1) +
+  geom_point(aes(col = nutrients_pool), size = 1.5) +
+  geom_errorbar(aes(ymin = low_rel, ymax = high_rel, col = nutrients_pool), 
+                width = 0.1, size = 0.1) +
+  geom_hline(yintercept = 0, linetype = 2, col = "lightgrey") +
+  facet_wrap(~ part, scales = "free_y", ncol = 2) + 
+  scale_color_manual(name = "Water column nutrients pool", 
+                     values = c("#ECCBAE", "#046C9A", "#D69C4E", "#ABDDDE", "#000000")) +
+  labs(x = "Number of fish individuals", 
+       y = expression(paste(Delta, Sigma, Biomass[null - AR], " (dry) [%]"))) + 
+  theme_classic(base_size = 10) + 
+  theme(legend.position = "bottom")
+
+# ggplot(data = biomass_diff, aes(x = pop_n, y = mean_abs)) + 
+#   geom_line(aes(group = nutrients_pool), alpha = 0.1) +
+#   geom_point(aes(col = nutrients_pool), size = 1.5) +
+#   geom_errorbar(aes(ymin = low_abs, ymax = high_abs, col = nutrients_pool), 
+#                 width = 0.1, size = 0.1) +
+#   geom_hline(yintercept = 0, linetype = 2, col = "lightgrey") +
+#   facet_wrap(~ part, scales = "free_y", nrow = 1) + 
+#   scale_color_viridis_d(name = "Starting water column nutrients pool") +
+#   labs(x = "Number of fish individuals", 
+#        y = expression(paste(Delta, Sigma, Biomass[rand - attr], " (dry) [%]"))) + 
+#   theme_classic(base_size = 10) + 
 #   theme(legend.position = "bottom")
-# 
-# 
-# #### Save ggplots ####
-# 
-# # set default arguments to save plots
-# width <- 297
-# 
-# height <- 120
-# 
-# dpi <- 300
-# 
-# units <- "mm"
-# 
-# suppoRt::save_ggplot(plot = gg_production_nutr, filename = "gg_production_nutr.png",
-#                      path = "04_Figures/03_simulation_experiment/",
-#                      width = width, height = height, dpi = dpi, units = units,
-#                      overwrite = FALSE)
+
+gg_production_full_design_sum <- ggplot(production_combined_sum, 
+                                        aes(x = pop_n, y = mean)) + 
+  geom_line(aes(group = interaction(id_move, nutrients_pool), linetype = id_move), 
+            col = "lightgrey") +
+  geom_point(aes(col = nutrients_pool, shape = id_move), size = 1.5) +
+  facet_wrap(~ part, scales = "free_y", ncol = 2) + 
+  scale_color_manual(name = "Water column nutrients pool",
+                     values = c("#ECCBAE", "#046C9A", "#D69C4E", "#ABDDDE", "#000000")) +
+  scale_shape_manual(name = "Hypothesis", values = c(1, 15)) +
+  scale_linetype_manual(name = "Hypothesis", values = c(2, 1)) +
+  labs(x = "Number of fish individuals", 
+       y = expression(paste(Sigma, Production, " (dry) [g]"))) + 
+  theme_classic(base_size = 10) + 
+  theme(legend.position = "bottom")
+
+production_diff <- dplyr::group_by(production_combined, id_move, part, nutrients_pool, pop_n) %>% 
+  dplyr::summarise(mean = mean(production), sd = sd(production), .groups = "drop") %>%
+  dplyr::mutate(low = mean - sd, high = mean + sd) %>% 
+  tidyr::pivot_wider(-sd, names_from = id_move, values_from = c(low, mean, high)) %>% 
+  purrr::set_names(names(.) %>% 
+                     stringr::str_replace_all(pattern = " ", replacement = "_") %>% 
+                     stringr::str_to_lower()) %>% 
+  dplyr::mutate(low_abs = low_attr - low_rand,
+                mean_abs = mean_attr - mean_rand, 
+                high_abs = high_attr - high_rand, 
+                low_rel = low_abs / low_rand * 100,
+                mean_rel = mean_abs / mean_rand * 100, 
+                high_rel = high_abs / high_rand * 100, 
+                part = factor(part, levels = c("ag_production", "bg_production"), 
+                              labels = c("Aboveground production", "Belowground production")))
+
+gg_production_full_design_diff <- ggplot(data = production_diff, 
+                                         aes(x = pop_n, y = mean_rel)) + 
+  geom_line(aes(group = nutrients_pool), alpha = 0.1) +
+  geom_point(aes(col = nutrients_pool), size = 1.5) +
+  geom_errorbar(aes(ymin = low_rel, ymax = high_rel, col = nutrients_pool), 
+                width = 0.1, size = 0.1) +
+  geom_hline(yintercept = 0, linetype = 2, col = "lightgrey") +
+  facet_wrap(~ part, scales = "free_y", ncol = 2) + 
+  scale_color_viridis_d(name = "Water column nutrients pool") +
+  labs(x = "Number of fish individuals", 
+       y = expression(paste(Delta, Sigma, Production[null - AR], " (dry) [%]"))) + 
+  theme_classic(base_size = 10) + 
+  theme(legend.position = "bottom")
+
+# ggplot(data = production_diff, aes(x = pop_n, y = mean_abs)) + 
+#   geom_line(aes(group = nutrients_pool), alpha = 0.1) +
+#   geom_point(aes(col = nutrients_pool), size = 1.5) +
+#   geom_errorbar(aes(ymin = low_abs, ymax = high_abs, col = nutrients_pool), 
+#                 width = 0.1, size = 0.1) +
+#   geom_hline(yintercept = 0, linetype = 2, col = "lightgrey") +
+#   facet_wrap(~ part, scales = "free_y", nrow = 1) + 
+#   scale_color_viridis_d(name = "Starting water column nutrients pool") +
+#   labs(x = "Number of fish individuals", 
+#        y = expression(paste(Delta, Sigma, Production[rand - attr], " (dry) [%]"))) + 
+#   theme_classic(base_size = 10) + 
+#   theme(legend.position = "bottom")
+
+#### Save ggplots ####
+
+# set default arguments to save plots
+width <- 210
+
+height <- 297 * 0.45
+
+dpi <- 300
+
+units <- "mm"
+
+overwrite <- FALSE
+
+suppoRt::save_ggplot(plot = gg_biomass_full_design_sum, 
+                     filename = "gg_biomass_full_design_sum.png",
+                     path = "04_Figures/03_simulation_experiment/",
+                     width = width, height = height, dpi = dpi, units = units,
+                     overwrite = overwrite)
+
+suppoRt::save_ggplot(plot = gg_biomass_full_design_diff, 
+                     filename = "gg_biomass_full_design_diff.png",
+                     path = "04_Figures/03_simulation_experiment/",
+                     width = width, height = height, dpi = dpi, units = units,
+                     overwrite = overwrite)
+
+suppoRt::save_ggplot(plot = gg_production_full_design_sum , 
+                     filename = "gg_production_full_design_sum.png",
+                     path = "04_Figures/03_simulation_experiment/",
+                     width = width, height = height, dpi = dpi, units = units,
+                     overwrite = overwrite)
+
+suppoRt::save_ggplot(plot = gg_production_full_design_diff, 
+                     filename = "gg_production_full_design_diff.png",
+                     path = "04_Figures/03_simulation_experiment/",
+                     width = width, height = height, dpi = dpi, units = units,
+                     overwrite = overwrite)
